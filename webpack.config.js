825da281a4b1a376a2c2347672fd7989e61f5996
张威 <zw900808@gmail.com>
2019-08-06 16:18:23 +0800

    发布 0.1.9 版本

--- webpack.config.js
+++ webpack.config.js
@@ -13,15 +13,15 @@ const isProd = process.env.NODE_ENV === "production";
 
 module.exports = {
   entry: { main: "./src/App.js" },
-  // output: {
-  //   // path: resolve("dist"), // 输出目录
-  //   path: path.resolve(__dirname, "dist"),
-  //   filename: "[name].js" // 输出文件
-  //   // libraryTarget: "umd" // 采用通用模块定义
-  //   // library: "hiynn-design", // 库名称
-  //   // libraryExport: "default", // 兼容 ES6(ES2015) 的模块系统、CommonJS 和 AMD 模块规范
-  //   // globalObject: "this" // 兼容node和浏览器运行，避免window is not undefined情况
-  // },
+  output: {
+    // path: resolve("dist"), // 输出目录
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name].js" // 输出文件
+    // libraryTarget: "umd" // 采用通用模块定义
+    // library: "hiynn-design", // 库名称
+    // libraryExport: "default", // 兼容 ES6(ES2015) 的模块系统、CommonJS 和 AMD 模块规范
+    // globalObject: "this" // 兼容node和浏览器运行，避免window is not undefined情况
+  },
   devtool: "#source-map",
   module: {
     rules: [
