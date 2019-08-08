a1a060e162e58b60bfde659fa9975bb91b8136f3
张威 <zw900808@gmail.com>
2019-08-08 13:53:49 +0800

    发布 0.1.39版本

--- webpack.config.js
+++ webpack.config.js
@@ -56,11 +56,11 @@ module.exports = {
   //压缩js
   optimization: {
     minimizer: [
-      // new UglifyJsPlugin({
-      //   cache: true,
-      //   parallel: true,
-      //   sourceMap: true
-      // }),
+      new UglifyJsPlugin({
+        cache: true,
+        parallel: true,
+        sourceMap: true
+      }),
       new OptimizeCssAssetsPlugin({
         assetNameRegExp: /\.css\.*(?!.*map)/g, //注意不要写成 /\.css$/g
         cssProcessor: require("cssnano"),
