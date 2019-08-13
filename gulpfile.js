aa0e4815e7ce8acb460820b7a9c3f082aeecb0e7
张威 <zw900808@gmail.com>
2019-08-13 17:19:24 +0800

    新增 copy-indexjs 的 gulp 任务,并生成 css.js 文件

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,11 @@
+const fs = require("fs");
 const gulp = require("gulp");
 const path = require("path");
 const minifycss = require("gulp-minify-css");
 const rename = require("gulp-rename");
 const postcss = require("gulp-postcss");
 const concat = require("gulp-concat");
+const replace = require("gulp-replace");
 const sass = require("gulp-sass");
 const sourcemaps = require("gulp-sourcemaps");
 const autoprefixer = require("gulp-autoprefixer");
@@ -16,8 +18,9 @@ const distDir = resolve("dist");
 const libDir = resolve("lib");
 const esDir = resolve("es");
 const sassDir = resolve("src/**/*.scss");
-const styleDir = resolve("src/**/index.scss");
+const indexJsDir = resolve("src/**/style/index.js");
 
+// 复制 sass 文件到 lib es 文件夹下
 gulp.task("copy-sass", () => {
   return gulp
     .src(sassDir)
@@ -27,6 +30,24 @@ gulp.task("copy-sass", () => {
     .pipe(gulp.dest(esDir));
 });
 
+// 根据 index.js 创建一个全新的 css.js 供按需加载 styel:'css' 使用
+gulp.task("copy-indexjs", () => {
+  return gulp
+    .src(indexJsDir)
+    .pipe(sourcemaps.init())
+    .pipe(replace("scss", "css"))
+    .pipe(
+      rename(function(path) {
+        path.basename = "css";
+        path.extname = ".js";
+      })
+    )
+    .pipe(sourcemaps.write("."))
+    .pipe(gulp.dest(libDir))
+    .pipe(gulp.dest(esDir));
+});
+
+// 编译 sass 文件到 es 和 lib 文件夹下
 gulp.task("compile-sass", () => {
   return gulp
     .src(sassDir)
@@ -39,6 +60,7 @@ gulp.task("compile-sass", () => {
     .pipe(gulp.dest(esDir));
 });
 
+// 编译 sass 到 dist 文件夹下
 gulp.task("dist", () => {
   return gulp
     .src(sassDir)
@@ -87,4 +109,4 @@ gulp.task("compile-with-lib", () => {
   );
 });
 
-gulp.task("compile", gulp.series(gulp.parallel("copy-sass", "compile-sass", "dist")));
+gulp.task("compile", gulp.series(gulp.parallel("copy-sass", "copy-indexjs", "compile-sass", "dist")));
