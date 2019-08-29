b37edccc2e4acbfbc2a68781c4eddc2952db962b
yingxiaxin <yingxiaxin@hiynn.com>
2019-08-29 15:07:02 +0800

    更新按需加载react-code-previewer等

--- scripts/webpack.config.dev.js
+++ scripts/webpack.config.dev.js
@@ -31,6 +31,10 @@ module.exports = {
   module: {
     rules: [
       {
+        test: /\.md$/,
+        use: "raw-loader"
+      },
+      {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
         use: {
--- src/pages/HdHGroupPage.js
+++ src/pages/HdHGroupPage.js
@@ -1,6 +1,13 @@
 import React, { Component } from 'react';
 import HGroup from '../../components/hd-hGroup';
 import '../../components/hd-hGroup/style/index';
+import { PreviewLayout, CodePreviewer, MdPreviewer } from 'react-code-previewer';
+// import 'react-code-previewer/es/code-previewer/style/css';
+// import 'react-code-previewer/es/preview-layout/style/css';
+// import 'react-code-previewer/es/markdown-previewer/style/css';
+
+import introMd from '../md/hd-hGroup/intro.md';
+import exampleMd from '../md/hd-hGroup/examples.md';
 
 export default class HdHGroupPage extends Component {
   constructor(props) {
@@ -12,48 +19,29 @@ export default class HdHGroupPage extends Component {
   }
 
   render() {
-    return [
-      <HGroup style={{ background: 'lightblue' }} key={1}>
-        <div>i'm a div</div>
-        <p>this is a p</p>
-        <span>span span...</span>
-      </HGroup>,
+    const demo1 = `<HGroup style={{ background: 'lightblue' }} key={1}>
+  <div>i'm a div</div>
+  <p>this is a p</p>
+  <span>span span...</span>
+</HGroup>`;
 
-      <HGroup width='900'
-        height='200'
-        horizontal-align='right'
-        vertical-align='top'
-        style={{ background: 'lightgray' }}
-        key={2}
-      >
-        <div>i'm a div</div>
-        <p>this is a p</p>
-        <span>span span...</span>
-      </HGroup>,
+    return (
+      <div style={{ padding: '2em' }}>
+        <PreviewLayout>
+          <MdPreviewer md={introMd}></MdPreviewer>
 
-      <HGroup width='1100'
-        height='200'
-        horizontal-align='justify'
-        vertical-align='center'
-        style={{ background: 'lightblue' }}
-        key={3}
-      >
-        <div>i'm a div</div>
-        <p>this is a p</p>
-        <span>span span...</span>
-      </HGroup>,
+          <MdPreviewer md={exampleMd}></MdPreviewer>
 
-      <HGroup width='50%'
-        height='200'
-        horizontal-align='between'
-        vertical-align='bottom'
-        style={{ background: 'lightgray' }}
-        key={4}
-      >
-        <div>i'm a div</div>
-        <p>this is a p</p>
-        <span>span span...</span>
-      </HGroup>,
-    ];
+          <CodePreviewer code={demo1} showCode={true}>
+            <HGroup style={{ background: 'lightblue' }} key={1}>
+              <div>i'm a div</div>
+              <p>this is a p</p>
+              <span>span span...</span>
+            </HGroup>
+          </CodePreviewer>
+
+        </PreviewLayout>
+      </div>
+    );
   }
 }
