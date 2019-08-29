2937cf244fc6d7ed48e466008712889130c50fae
xushanpei <xu_shan_pei@163.com>
2019-08-29 17:33:07 +0800

    增加验证

--- components/hd-formValidation/index.js
+++ components/hd-formValidation/index.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Layout, Menu, Icon, Avatar, Dropdown, Spin } from "antd";
+import { Layout, Menu, Icon, Avatar, Dropdown, Spin, Form, Input, Button, Checkbox, message } from "antd";
 import { fromJs, Map, Set } from "immutable";
 import PropTypes from "prop-types";
 import cls from "classnames";
@@ -13,14 +13,65 @@ class HdFormValidation extends Component {
     this.state = {};
   }
 
-  render() {
-    let styleContext = this.context;
-    console.log(styleContext);
+  handleSubmit = e => {
+    e.preventDefault();
+    this.props.form.validateFields((err, values) => {
+      if (!err) {
+        console.log("Received values of form: ", values);
+      }
+    });
+  };
 
-    return <div>{this.props.children}</div>;
+  render() {
+    const { getFieldDecorator } = this.props.form;
+    return (
+      <div>
+        <Form onSubmit={this.handleSubmit} className="login-form">
+          {this.props.HdData.map((item, index) => {
+            //  return
+            console.log(item, index);
+            return (
+              <Form.Item key={index} label={item.label} labelCol={{ span: item.labelCol }} wrapperCol={{ span: item.wrapperCol }}>
+                {getFieldDecorator(`${item.key}`, {
+                  rules: [
+                    { required: item.required, message: `请输入${item.label}` },
+                    { pattern: item.reg, message: item.message }
+                    // {
+                    //   validator: (rule, value, callback, source, options) => {
+                    //     let patten = new RegExp(item.reg);
+                    //     if (!patten.test(value)) {
+                    //       let key = item.key;
+                    //       console.log(key);
+                    //       // message.warning("输入有误");
+                    //       this.props.form.setFieldsValue({
+                    //         [key]: value.substring(0, value.length - 1)
+                    //       });
+                    //       return callback("输入有误");
+                    //     } else {
+                    //       // return callback("输入有误!!!");
+                    //     }
+                    //   }
+                    // }
+                  ]
+                })(<Input type={`${item.type}`} prefix={<Icon type="user" style={{ color: "rgba(0,0,0,.25)" }} />} placeholder={item.placeholder} />)}
+              </Form.Item>
+            );
+          })}
+          <Button type="primary" htmlType="submit" className="login-form-button">
+            Log in
+          </Button>
+        </Form>
+      </div>
+    );
   }
 }
 
-HdFormValidation.contextType = StyleContext;
+HdFormValidation = Form.create({
+  name: "normal_login",
+  onValuesChange(props, changedValues, allValues) {
+    // console.log(allValues);
+    props.Hdchange(allValues);
+  }
+})(HdFormValidation);
 
 export default HdFormValidation;
--- src/pages/HdFormValidationPages.js
+++ src/pages/HdFormValidationPages.js
@@ -1,12 +1,50 @@
 import React, { Component } from "react";
 import HdFormValidation from "../../components/hd-formValidation";
+import { Input, message } from "antd";
 
 class HdFormValidationPages extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: [
+        {
+          key: "username",
+          label: "用户名",
+          type: "text",
+          labelCol: 3,
+          wrapperCol: 10,
+          reg: /^[a-zA-Z0-9 ]{0,10}$/,
+          placeholder: "请输入用户名",
+          required: true,
+          message: "输入的格式只能为字母和数字,且长度0-10之间"
+        },
+        {
+          key: "password",
+          label: "测试2",
+          type: "password",
+          labelCol: 3,
+          wrapperCol: 10,
+          reg: /^[a-zA-Z0-9 ]{0,10}$/,
+          placeholder: "请输入密码",
+          required: true,
+          validator: false
+        }
+      ]
+    };
+  }
+
+  onChange = data => {
+    console.log(data);
+  };
+  ok = data => {
+    console.log(data);
+  };
+
   render() {
     return (
-      <HdFormValidation>
-        <div>this is HdFormValidation Page</div>
-      </HdFormValidation>
+      <div>
+        <HdFormValidation Hdchange={this.onChange} HdData={this.state.data} HdOk={this.ok}></HdFormValidation>
+      </div>
     );
   }
 }
