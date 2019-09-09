0964d513393bc984a451faa8f98b58cfaa9b5c4c
yingxiaxin <yingxiaxin@hiynn.com>
2019-09-09 14:22:04 +0800

    移除codeArea组件内容

--- components/index.js
+++ components/index.js
@@ -6,8 +6,7 @@ import HdArticle from "./article";
 import HdComment from "./comment";
 import HGroup from "./hgroup";
 import VGroup from "./vGroup";
-import CodeArea from "./codeArea";
 import Panel from "./panel";
 
-export { HdLayout, HdMaster, HdMenus, HdCard, HdArticle, HdComment, HGroup, VGroup, CodeArea, Panel };
-export default { HdLayout, HdMaster, HdMenus, HdCard, HdArticle, HdComment, HGroup, VGroup, CodeArea, Panel };
+export { HdLayout, HdMaster, HdMenus, HdCard, HdArticle, HdComment, HGroup, VGroup, Panel };
+export default { HdLayout, HdMaster, HdMenus, HdCard, HdArticle, HdComment, HGroup, VGroup, Panel };
